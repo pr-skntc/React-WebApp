8cfdb3dc92a7401b8abd1404ae462406c187fd55
hanaseed191 <hanaseed191@gmail.com>
2025-08-21 17:49:28 +0700

    21/8/2568

---
+++ src/Pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <>
+      <div className="p-6 bg-white rounded-md shadow-sm">
+        <h1 className="font-bold text-2xl mb-3">About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+          corrupti quisquam ipsam similique corporis alias quidem nam quasi,
+          sint odio sed nemo accusantium voluptas harum dolore consequatur,
+          deserunt error. Ratione qui voluptatibus dignissimos natus nobis.
+          Eligendi doloremque, perferendis modi soluta, quod placeat earum
+          beatae officia, eveniet laborum rem cumque commodi!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default AboutPage;
