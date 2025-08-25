958fee91f52304ae23c4f7c538316b15a7978812
hanaseed191 <hanaseed191@gmail.com>
2025-08-25 17:25:22 +0700

    25/8/2568 17:25

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import NewTask from "../Components/NewTask";
 import TodoItem from "../Components/TodoItem";
 function HomePage() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]); //เก็บรายการ To-do ทั้งหมดเป็น array 
 
-  const addTask = (task) => {
-    setTodos((prevTodos) => [...prevTodos, task]);
+  const addTask = (task) => {  
+    setTodos((prevTodos) => [...prevTodos, task]); //เพิ่มงานใหม่ลงใน array ของ todos และคัดลอกงานเก่าๆ มาไว้ด้วย
   };
   return (
     <>
-      <NewTask addTask={addTask} />
+      <NewTask addTask={addTask} />  
+      {/*ส่งฟังก์ชัน addTask ไปยังคอมโพเนนต์ NewTask เพื่อให้ NewTask สามารถเพิ่มงานใหม่ได้*/}
       <ul className="bg-gray-200 rounded-md shadow-sm p-4">
-        {todos.map((todo, i) => (
-          <TodoItem key={i} id={i} todo={todo} />
+        {todos.map((todo, i) => (  //วนลูปผ่าน array ของ todos 
+          <TodoItem key={i} id={i} todo={todo} />  //ส่งค่า todo และดัชนี i ไปยังคอมโพเนนต์ TodoItem  
         ))}
       </ul>
     </>
